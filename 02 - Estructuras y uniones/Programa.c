d36a5e0cface4b5dcbcc1d6ad56932eb39521876
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2025-06-11 12:00:35 -0600

    Sesión 9 - 11/06/2025

--- 02 - Estructuras y uniones/Programa.c
+++ 02 - Estructuras y uniones/Programa.c
@@ -1,7 +1,7 @@
 //  Directivas de Preprocesador
 #include <stdio.h>      // archivos de cabecera o .h
 #include <stdlib.h>
-#include <string.h>     // PAra la gestion de cadenas
+#include <string.h>     // Para la gestion de cadenas
 
 #define pausa system("pause")
 #define cls system("cls")
@@ -27,7 +27,7 @@ typedef struct Datos datos;  // Formalización del tipo de datos "datos"
 int main()  // Punto de entrada
 { 
     int contador;
-    datos miPersona;
+    datos miPersona[5];
     /*printf("El tamaño de la estructura Persona es: %d bytes\n", sizeof(struct Persona) );
     printf("El tamaño de datos es: %d bytes\n", sizeof(datos) );
     printf("El tamaño de persona es: %d bytes\n", sizeof(miPersona) );
@@ -36,22 +36,27 @@ int main()  // Punto de entrada
     printf("El tamaño de float es: %d bytes\n", sizeof(float) );
     printf("El tamaño de double es: %d bytes\n", sizeof(double) );*/
 
+    
     printf("Dame el nombre: ");
-    gets(miPersona.nombrePersona.Nombre);
+    gets(miPersona[2].nombrePersona.Nombre);
     printf("Dame el apellido: ");
-    gets(miPersona.nombrePersona.Apellido);
+    gets(miPersona[2].nombrePersona.Apellido);
     printf("Dame la edad: ");
-    scanf("%d", &miPersona.edad);
+    scanf("%d", &miPersona[2].edad);
     printf("Dame la altura: ");
-    scanf("%f", &miPersona.altura);
+    scanf("%f", &miPersona[2].altura);
     printf("Dame el sexo: ");
     fflush(stdin);
-    scanf("%c", &miPersona.sexo);
+    scanf("%c", &miPersona[2].sexo);
     
-    printf("El nombre de la persona es %s %s\n", miPersona.nombrePersona.Nombre, miPersona.nombrePersona.Apellido);
-    printf("La edad de la persona es %d\n", miPersona.edad);
-    printf("La altura de la persona es %f\n", miPersona.altura);   
-    printf("El sexo de la persona es %c\n", miPersona.sexo);
+    printf("El nombre de la persona es %s %s\n", miPersona[1].nombrePersona.Nombre, miPersona[2].nombrePersona.Apellido);
+    printf("La edad de la persona es %d\n", miPersona[1].edad);
+    printf("La altura de la persona es %f\n", miPersona[1].altura);   
+    printf("El sexo de la persona es %c\n", miPersona[1].sexo);
+    printf("El nombre de la persona es %s %s\n", miPersona[2].nombrePersona.Nombre, miPersona[2].nombrePersona.Apellido);
+    printf("La edad de la persona es %d\n", miPersona[2].edad);
+    printf("La altura de la persona es %f\n", miPersona[2].altura);   
+    printf("El sexo de la persona es %c\n", miPersona[2].sexo);
 
     pausa; 
 }
